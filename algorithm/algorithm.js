15e83f8513241749d3e12b32d6f3839df31ecf81
LucyLee <253803923@qq.com>
2023-03-01 23:15:36 +0800

    update configuration

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -1,29 +1,27 @@
-let nums = [2, 3, 1, 0, 2, 5, 3]
-
-// 法1
-
-function swap(a, b) {
-    let t = a;
-    a = b;
-    b = t;
-}
-let set = new Set();
-    
-let res = (function find(nums) {
-    for (let i = 0; i < 7; i++) {
-        // if (nums[i] < i)    return nums[i];
-        if (nums[i] === i) continue;
-        if (nums[i] === nums[nums[i]]) return nums[i];
-        else {
-           
-            let tmp = nums[i]
-            [nums[i], nums[nums[i]]] = [nums[nums[i]], nums[i]]
-            console.log(nums)
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    // let map = new Map()
+    let arr = []
+    for (let val of s.split('')) {
+        if (val == '(' || val == '[' || val == '{') arr.push(val)
+        else if (val = ')') {
+            if (arr[arr.length - 1] != '(') return false
+            else arr.pop()
         }
-    
+        else if (val = ']') {
+            if (arr[arr.length - 1] != '[') return false
+            else arr.pop()
+        }
+        else {
+            if (arr[arr.length - 1] != '{') return false
+            else arr.pop()
+        }
+        console.log(arr)
     }
-})();
+    return arr.length == 0
+};
 
-console.log(res)
-
-    
+console.log(isValid('()[]{}'))
