b7f524b7113b501968e1bc0590103906319a3f1b
LucyLee <253803923@qq.com>
2023-03-07 15:07:16 +0800

    udpate

--- algorithm.js
+++ algorithm.js
@@ -1,25 +1,27 @@
 /**
- * @param {string} s
+ * @param {number[]} nums
+ * @param {number} k
  * @return {number}
  */
-var minimumDeletions = function(s) {
-    const len = s.length
-    let res = 0
-    let stk = []
-    // stk[0] = s.substr(0, 1)
-    s.split('').forEach(val => {
-        if (stk[stk.length - 1] == 'b' && val == 'a') {
-            stk.pop()
-            res++
-        } else {
-            stk.push(val)
+var subarraySum = function(nums, k) {
+    const len = nums.length
+    let cnt = 0
+    for (let i = 0; i < len; i++) {
+        let sum = 0
+        for(let j = i; j < len; j ++) {
+            sum += nums[j]
+            if (sum == k) {
+                if (j != len - 1 && nums[j + 1] == 0) cnt++
+                else {
+                    cnt++ 
+                    break
+                }
+            }
         }
-        
-        console.log(stk)
-    })
-    return res
+    }
+    return cnt
 };
 
-const arr = [1,4,4]
+const arr = [1,-1,0]
 
-console.log(minimumDeletions("bbaaaaabb"))
+console.log(subarraySum(arr, 0))
