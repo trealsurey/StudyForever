57c6ca0e9ddb5752eb067945b978421094d8fbc4
LucyLee <253803923@qq.com>
2023-04-26 00:31:44 +0800

    add new pics and update

--- js/CommonJS样例/main.js
+++ js/CommonJS样例/main.js
@@ -1,13 +1,4 @@
-// const util = require("./util")
+const path = require("path");
+const filePath = "F://Notes/test.txt";
 
-// console.log(util.UTIL_NAME);
-
-// util.getData()
-
-// 导入时直接进行解构，免去每次调用都要加 .util
-
-const { UTIL_NAME, getData } = require("./util");
-
-console.log(UTIL_NAME);
-
-getData();
+console.log(path.extname(filePath));
